4228371d7319c119d1c5d3a7cec126d499052c54
Colibri-Vert <dalexu2021@outlook.com>
2024-08-25 16:18:21 -0400

    Selector panel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,15 @@ import banners from "../lib/banners"
 import characters, { type Character } from "../lib/characters"
 import lightCones, { type LightCone } from "../lib/light-cones"
 
-enum State { ICON = -1, NULL = 0 }
+enum FeatureType { CHARACTER, LIGHT_CONE }
 enum SortType { FIRST_RELEASE, LAST_RERUN }
 
-type TableData<T extends Character | LightCone> = [T, number[], number, number]
+enum State { ICON = -1, NULL = 0 }
+type TableData<T extends Character | LightCone> = [T, State[], number, number]
 
 let versions: string[] = []
+
+let type: FeatureType = FeatureType.CHARACTER
 let sort: SortType = SortType.FIRST_RELEASE
 
 let fiveStarCharacters: TableData<Character>[] = []
@@ -59,7 +62,7 @@ onMount(() =>
 
 function process<T extends Character | LightCone>(features: T[], banners: [string, number][]): TableData<T>[]
 {
-    let data: { [key: string]: [T, number[]] } = {}
+    let data: { [key: string]: [T, State[]] } = {}
     for (let feature of features)
     {
         // Assign each feature a row of cells (initialized to the null state)
@@ -145,35 +148,47 @@ function map(x: number): number
     <title>HSR Banner History</title>
 </svelte:head>
 <div>
-    <div class="title">
-        <h1>Honkai: Star Rail Banner History</h1>
-        <form>
-            <span>Sort by:</span>
-            <select bind:value={sort} on:change={() => sortFeatures(sort)}>
-                <option value={SortType.FIRST_RELEASE}>First Release</option>
-                <option value={SortType.LAST_RERUN}>Last Rerun</option>
-            </select>
-        </form>
+    <div class="header">
+        <div class="title">
+            <img src="{base}/favicon.png" alt="">
+            <h1>Honkai: Star Rail Banner History</h1>
+        </div>
+        <div class="sort-form">
+            <div class="select">
+                <button class="select-option {type === FeatureType.CHARACTER ? "active" : ""}"
+                    on:click={() => type = FeatureType.CHARACTER}><span>Characters</span></button>
+                <button class="select-option {type === FeatureType.LIGHT_CONE ? "active" : ""}"
+                    on:click={() => type = FeatureType.LIGHT_CONE}><span>Light Cones</span></button>
+            </div>
+            <div class="select">
+                <button class="select-option {sort === SortType.FIRST_RELEASE ? "active" : ""}"
+                    on:click={() => sortFeatures(sort = SortType.FIRST_RELEASE)}><span>First Release</span></button>
+                <button class="select-option {sort === SortType.LAST_RERUN ? "active" : ""}"
+                    on:click={() => sortFeatures(sort = SortType.LAST_RERUN)}><span>Last Rerun</span></button>
+            </div>
+        </div>
     </div>
     <div class="main">
         <table class="character-names">
-            <thead><th class="header"><span>Version</span></th></thead>
+            <thead><th class="feature"><span>Version</span></th></thead>
             <tbody>
-                {#each fiveStarCharacters as [character] (character.id)}
-                    <tr><td class="header"><span>{character.name}</span></td></tr>
-                {/each}
-                <tr><td class="header"></td></tr>
-                {#each fourStarCharacters as [character] (character.id)}
-                    <tr><td class="header"><span>{character.name}</span></td></tr>
-                {/each}
-                <tr><td class="header"></td></tr>
-                {#each fiveStarLightCones as [lightCone] (lightCone.id)}
-                    <tr><td class="header"><span>{lightCone.name}</span></td></tr>
-                {/each}
-                <tr><td class="header"></td></tr>
-                {#each fourStarLightCones as [lightCone] (lightCone.id)}
-                    <tr><td class="header"><span>{lightCone.name}</span></td></tr>
-                {/each}
+                {#if type === FeatureType.CHARACTER}
+                    {#each fiveStarCharacters as [character] (character.id)}
+                        <tr><td class="feature"><span>{character.name}</span></td></tr>
+                    {/each}
+                    <tr><td class="feature"></td></tr>
+                    {#each fourStarCharacters as [character] (character.id)}
+                        <tr><td class="feature"><span>{character.name}</span></td></tr>
+                    {/each}
+                {:else if type === FeatureType.LIGHT_CONE}
+                    {#each fiveStarLightCones as [lightCone] (lightCone.id)}
+                        <tr><td class="feature"><span>{lightCone.name}</span></td></tr>
+                    {/each}
+                    <tr><td class="feature"></td></tr>
+                    {#each fourStarLightCones as [lightCone] (lightCone.id)}
+                        <tr><td class="feature"><span>{lightCone.name}</span></td></tr>
+                    {/each}
+                {/if}
             </tbody>
         </table>
         <div class="body">
@@ -184,61 +199,63 @@ function map(x: number): number
                     {/each}
                 </thead>
                 <tbody>
-                    {#each fiveStarCharacters as [character, timeline] (character.id)}
-                        <tr>
-                            {#each timeline as n}
-                                {#if n === State.ICON}
-                                    <td><img src="{base}/characters/{character.id}.png" alt=""></td>
-                                {:else if n !== State.NULL}
-                                    <td style={`background-color: hsl(${colors[character.element]}%, ${map(n - 1) * 100}%)`}>
-                                        <span>{n}</span>
-                                    </td>
-                                {:else}<td></td>{/if}
-                            {/each}
-                        </tr>
-                    {/each}
-                    <tr>{#each { length: versions.length * 2 } as _}<td></td>{/each}</tr>
-                    {#each fourStarCharacters as [character, timeline] (character.id)}
-                        <tr>
-                            {#each timeline as n}
-                                {#if n === State.ICON}
-                                    <td><img src="{base}/characters/{character.id}.png" alt=""></td>
-                                {:else if n !== State.NULL}
-                                    <td style={`background-color: hsl(${colors[character.element]}%, ${map(n - 1) * 100}%)`}>
-                                        <span>{n}</span>
-                                    </td>
-                                {:else}<td></td>{/if}
-                            {/each}
-                        </tr>
-                    {/each}
-                    <tr>{#each { length: versions.length * 2 } as _}<td></td>{/each}</tr>
-                    {#each fiveStarLightCones as [lightCone, timeline] (lightCone.id)}
-                        <tr>
-                            {#each timeline as n}
-                                {#if n === State.ICON}
-                                    <td><img src="{base}/light-cones/{lightCone.id}.png" alt=""></td>
-                                {:else if n !== State.NULL}
-                                    <td style={`background-color: hsl(${colors["ice"]}%, ${map(n - 1) * 100}%)`}>
-                                        <span>{n}</span>
-                                    </td>
-                                {:else}<td></td>{/if}
-                            {/each}
-                        </tr>
-                    {/each}
-                    <tr>{#each { length: versions.length * 2 } as _}<td></td>{/each}</tr>
-                    {#each fourStarLightCones as [lightCone, timeline] (lightCone.id)}
-                        <tr>
-                            {#each timeline as n}
-                                {#if n === State.ICON}
-                                    <td><img src="{base}/light-cones/{lightCone.id}.png" alt=""></td>
-                                {:else if n !== State.NULL}
-                                    <td style={`background-color: hsl(${colors["ice"]}%, ${map(n - 1) * 100}%)`}>
-                                        <span>{n}</span>
-                                    </td>
-                                {:else}<td></td>{/if}
-                            {/each}
-                        </tr>
-                    {/each}
+                    {#if type === FeatureType.CHARACTER}
+                        {#each fiveStarCharacters as [character, timeline] (character.id)}
+                            <tr>
+                                {#each timeline as n}
+                                    {#if n === State.ICON}
+                                        <td><img src="{base}/characters/{character.id}.png" alt=""></td>
+                                    {:else if n !== State.NULL}
+                                        <td style={`background-color: hsl(${colors[character.element]}%, ${map(n - 1) * 100}%)`}>
+                                            <span>{n}</span>
+                                        </td>
+                                    {:else}<td></td>{/if}
+                                {/each}
+                            </tr>
+                        {/each}
+                        <tr>{#each { length: versions.length * 2 } as _}<td></td>{/each}</tr>
+                        {#each fourStarCharacters as [character, timeline] (character.id)}
+                            <tr>
+                                {#each timeline as n}
+                                    {#if n === State.ICON}
+                                        <td><img src="{base}/characters/{character.id}.png" alt=""></td>
+                                    {:else if n !== State.NULL}
+                                        <td style={`background-color: hsl(${colors[character.element]}%, ${map(n - 1) * 100}%)`}>
+                                            <span>{n}</span>
+                                        </td>
+                                    {:else}<td></td>{/if}
+                                {/each}
+                            </tr>
+                        {/each}
+                    {:else if type === FeatureType.LIGHT_CONE}
+                        {#each fiveStarLightCones as [lightCone, timeline] (lightCone.id)}
+                            <tr>
+                                {#each timeline as n}
+                                    {#if n === State.ICON}
+                                        <td><img src="{base}/light-cones/{lightCone.id}.png" alt=""></td>
+                                    {:else if n !== State.NULL}
+                                        <td style={`background-color: hsl(${colors["ice"]}%, ${map(n - 1) * 100}%)`}>
+                                            <span>{n}</span>
+                                        </td>
+                                    {:else}<td></td>{/if}
+                                {/each}
+                            </tr>
+                        {/each}
+                        <tr>{#each { length: versions.length * 2 } as _}<td></td>{/each}</tr>
+                        {#each fourStarLightCones as [lightCone, timeline] (lightCone.id)}
+                            <tr>
+                                {#each timeline as n}
+                                    {#if n === State.ICON}
+                                        <td><img src="{base}/light-cones/{lightCone.id}.png" alt=""></td>
+                                    {:else if n !== State.NULL}
+                                        <td style={`background-color: hsl(${colors["ice"]}%, ${map(n - 1) * 100}%)`}>
+                                            <span>{n}</span>
+                                        </td>
+                                    {:else}<td></td>{/if}
+                                {/each}
+                            </tr>
+                        {/each}
+                    {/if}
                 </tbody>
             </table>
         </div>
@@ -251,6 +268,7 @@ function map(x: number): number
     --border-color:     #383838;
     --text-primary:     #ededed;
     --text-secondary:   #b3b3b3;
+    --active-color:     #8dbbe2;
 }
 
 :global(*) {
@@ -264,15 +282,25 @@ function map(x: number): number
     background-color: var(--background-color);
 }
 
-.title {
+.header {
     padding: 16px 48px;
     border-bottom: 1px solid var(--border-color);
+}
+
+.title {
+    display: flex;
+    align-items: end;
+    margin-bottom: 8px;
+    gap: 8px;
+}
+
+.title img {
+    height: 64px;
 }
 
 .title h1 {
     color: var(--text-primary);
     font-size: 1.8rem;
-    margin-bottom: 4px;
 }
 
 span {
@@ -280,14 +308,31 @@ span {
     font-size: 1rem;
 }
 
-select {
-    margin-left: 4px;
-    padding: 2px;
-    border-radius: 0;
+.sort-form {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px 16px;
 }
 
-option {
-    border-radius: 0;
+.select {
+    display: flex;
+}
+
+.select-option {
+    all: unset;
+    flex-shrink: 0;
+    padding: 4px 6px;
+    border: 1px solid var(--border-color);
+    cursor: pointer;
+}
+
+.select-option.active {
+    background: var(--active-color);
+    border: 1px solid var(--active-color);
+}
+
+.select-option.active > span {
+    color: var(--surface-color);
 }
 
 .main {
@@ -341,21 +386,21 @@ td > img {
     object-position: top;
 }
 
-.header, th {
+.feature, th {
     background-color: var(--surface-color);
 }
 
-.header  {
-    min-width: 320px;
+.feature  {
+    white-space: nowrap;
     padding: 0 24px;
     border: 1px solid var(--border-color);
 }
 
-td.header > span {
+td.feature > span {
     color: var(--text-secondary);
 }
 
-@media (max-width:540px) {
+@media (max-width: 600px) {
     .character-names {
         display: none;
     }
